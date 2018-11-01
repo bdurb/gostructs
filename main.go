828eda239d24f970db6f33f2aee3560f4a036854
bdurb <brian@rpmgreenville.com>
2018-11-01 12:00:55 -0700

    various ways to define a struct and assign values

--- main.go
+++ main.go
@@ -1,10 +1,18 @@
 package main
 
+import "fmt"
+
 type person struct {
 	firstName string
 	lastName  string
 }
 
 func main() {
+	var alex person
 
+	alex.firstName = "Alex"
+	alex.lastName = "Anderson" // dot notation to assign values
+	// alex := person{firstName: "Alex", lastName: "Anderson"} to create new struct person alex
+	fmt.Println(alex) // prints the struct
+	// fmt.Printf("%+v", alex) shows values from struct.
 }
