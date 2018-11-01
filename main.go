c2310efdc1dacde55eecb77e613070fca2e41b16
bdurb <brian@rpmgreenville.com>
2018-11-01 12:06:15 -0700

    examples of embedded structs

--- main.go
+++ main.go
@@ -2,17 +2,35 @@ package main
 
 import "fmt"
 
+type contactInfo struct {
+	email   string
+	zipCode int
+}
+
 type person struct {
 	firstName string
 	lastName  string
+	contact   contactInfo
 }
 
-func main() {
-	var alex person
+// func main() {
+// 	var alex person
 
-	alex.firstName = "Alex"
-	alex.lastName = "Anderson" // dot notation to assign values
-	// alex := person{firstName: "Alex", lastName: "Anderson"} to create new struct person alex
-	fmt.Println(alex) // prints the struct
-	// fmt.Printf("%+v", alex) shows values from struct.
+// 	alex.firstName = "Alex"
+// 	alex.lastName = "Anderson" // dot notation to assign values
+// 	// alex := person{firstName: "Alex", lastName: "Anderson"} to create new struct person alex
+// 	fmt.Println(alex) // prints the struct
+// 	// fmt.Printf("%+v", alex) shows values from struct.
+// }
+
+func main() {
+	jim := person{
+		firstName: "Jim",
+		lastName:  "Parsons",
+		contact: contactInfo{
+			email:   "jim@parsons.com",
+			zipCode: 89000,
+		},
+	}
+	fmt.Println(jim)
 }
